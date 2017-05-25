95c395278d0a2d84e0deb41edadc1bbcac93f055
Joe <joecheng1@livemail.tw>
2017-05-25 16:55:05 +0800

    no message

--- page3.js
+++ page3.js
@@ -1,6 +1,26 @@
+// $(document).ready(function() {
+// 		$('#fullpage').fullpage({})
+// 	});
+
 $('.wrapper a img').mouseenter(function(){
 	$(this).addClass('large')
 });
 $('.wrapper a img').mouseleave(function(){
 	$(this).removeClass('large')
-})
+});
+$('#goPrevious').click(function(){
+	$('body').fadeOut('slow',function(){
+		window.location.assign('project.html');
+	});
+});
+
+// $('body').html("<div>home.html</div>")
+
+
+
+
+// $('#goPrevious').click(function(){
+// 	$('body').fadeOut('slow',function(){
+// 		$('body').load('Home.html');
+// 	});
+// });
